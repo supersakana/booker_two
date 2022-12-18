f1201a065e7a0facee8de8ce8aa3edb0616a23f3
supersakana <zacwilliamson@icloud.com>
2022-12-18 16:42:23 -0600

    added date and time to flights model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,10 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights,
+           foreign_key: 'from_id',
+           class_name: 'Flight',
+           dependent: :destroy
+  has_many :arriving_flights,
+           foreign_key: 'to_id',
+           class_name: 'Flight',
+           dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_175002) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_223807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_175002) do
     t.bigint "to_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
+    t.time "time"
     t.index ["from_id"], name: "index_flights_on_from_id"
     t.index ["to_id"], name: "index_flights_on_to_id"
   end
