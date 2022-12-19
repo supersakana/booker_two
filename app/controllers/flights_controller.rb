2b608533262f04f7565d8406c4884914314925a8
supersakana <zacwilliamson@icloud.com>
2022-12-19 12:11:25 -0600

    added search result form and search class method for flights model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map { |a| ["#{a.code}, #{a.location}", a.id] }
-    @flights = Flight.all
+    return if search_params.empty?
+
+    @search_results = Flight.search(search_params)
+  end
+
+  private
+
+  def search_params
+    params.permit(:from_id, :to_id, :date)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,11 @@
 class Flight < ApplicationRecord
   belongs_to :from, class_name: 'Airport'
   belongs_to :to, class_name: 'Airport'
+
+  def self.search(params)
+    return if params[:date].empty?
+
+    where(['date = ? and from_id = ? and to_id = ?',
+           params[:date], params[:from_id], params[:to_id]])
+  end
 end
