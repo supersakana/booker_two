4a532150bf7708ffa3301fd05f7cb40aa4e74411
supersakana <zacwilliamson@icloud.com>
2022-12-19 13:24:40 -0600

    added passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_223807) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_192027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_223807) do
     t.time "time"
     t.index ["from_id"], name: "index_flights_on_from_id"
     t.index ["to_id"], name: "index_flights_on_to_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "flights", "airports", column: "from_id"
