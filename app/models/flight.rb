1e632a554b9530998eb5a713c0b8a63a41011ee4
supersakana <zacwilliamson@icloud.com>
2022-12-19 15:28:50 -0600

    added associations between bookings, passengers, and flights. Generated booking controller and bookings#new view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from, class_name: 'Airport'
   belongs_to :to, class_name: 'Airport'
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   def self.search(params)
     where(['date = ? and from_id = ? and to_id = ?',
