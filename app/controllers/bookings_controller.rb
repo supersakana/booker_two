bc297a13f4dab8792bbf23a361e806dc33b49659
supersakana <zacwilliamson@icloud.com>
2022-12-20 07:49:06 -0600

    when a user successfully books their flight, redirected to booking#show page with flight/passengeer information

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,34 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show]
+
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
     @p_num = params[:passenger_count].to_i
     @p_num.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      flash[:notice] = 'Booking successfully completed!'
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+      render json: @booking.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[id name email])
   end
 end
