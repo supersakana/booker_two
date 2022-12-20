5dd0da096d7ef8ac7216b6d0f314496e29ba0732
supersakana <zacwilliamson@icloud.com>
2022-12-20 14:18:19 -0600

    user is sent an email when booking is created (development env only)

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  def booking_confirmation
+    @booking = params[:booking]
+
+    @booking.passengers.each do |passenger|
+      mail(to: passenger.email, subject: 'Your flight is booked!')
+    end
+  end
 end
