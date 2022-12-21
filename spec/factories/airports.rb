94746024dc2192b745188a1f792b215a1251610d
supersakana <zacwilliamson@icloud.com>
2022-12-21 14:03:17 -0600

    installed factory bot

---
+++ spec/factories/airports.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :airport do
+    trait :atlanta do
+      location { 'Atlanta, Georgia' }
+      code { 'ATL' }
+    end
+
+    trait :norfolk do
+      location { 'Norfolk, Virginia' }
+      code { 'ORF' }
+    end
+  end
+end
---
+++ spec/factories/flights.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :flight do
+      trait :atl_orf do
+        datetime { "#{Date.tomorrow} '08:00AM'" }
+      end
+    end
+  end
