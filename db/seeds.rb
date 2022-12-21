5262ca72c98bf32cb0a822294a4d9a6a71d60074
supersakana <zacwilliamson@icloud.com>
2022-12-21 14:46:50 -0600

    added seed to db for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ Airport.create(code: 'ATL', location: 'Atlanta, Georgia')
 Airport.create(code: 'ORF', location: 'Norfolk, Virginia')
 
 today = Date.today.strftime('%Y-%m-%d')
-tomorow = (Date.today + 1).strftime('%Y-%m-%d')
+ten_days_later = (Date.today + 10).strftime('%Y-%m-%d')
 
-(today..tomorow).to_a.each do |d|
+(today..ten_days_later).to_a.each do |d|
   ['8:00AM', '12:00PM', '6:00PM'].each do |t|
     Flight.create(from_id: 1, to_id: 2, date: d, time: t)
     Flight.create(from_id: 2, to_id: 1, date: d, time: t)
